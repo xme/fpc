be255faba881b73b60b51d802d567ea196258ecd
Xavier Mertens <xavier@rootshell.be>
2018-07-13 13:34:54 +0200

    SANSFire edition

--- sensor/entrypoint.sh
+++ sensor/entrypoint.sh
@@ -1,70 +1,34 @@
 #!/bin/bash
-set -e
-if [ -r /.firstboot ]; then
-        if [ -z "$PCAP_INTERFACE" ]; then
-                export PCAP_INTERFACE=any
-        fi
-        if [ -z "$PCAP_CAPTURE_SIZE" ]; then
-                export PCAP_CAPTURE_SIZE=0
-        fi
-        if [ -z "$PCAP_FILE_SIZE" ]; then
-                export PCAP_FILE_SIZE=1000
-        fi
-        if [ -z "$PCAP_FILE_ROTATE" ]; then
-                export PCAP_FILE_ROTATE=100
-        fi
-        if [ -z "$PCAP_BPF_FILTER" ]; then
-                export PCAP_BPF_FILTER=""
-        fi
-        if [ -z "$PCAP_SENSOR_NAME" ]; then
-                # Generate random sensor name if none provided
-                export PCAP_SENSOR_NAME="sensor-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`"
-        fi
-        if [ -z "$SCP_ARGUMENTS" ]; then
-                export SCP_ARGUMENTS=""
-        fi
-        if [ -z "$SCP_TARGET" ]; then
-                echo "[ERROR] No target specified for scp (\$SCP_TARGET)"
-                exit 1
-        fi
-
-        # Generate a SSH keypair
-        ssh-keygen -b 4096 -t rsa -q -N "" -f /root/.ssh/id_rsa
-        if [ "$?" != "0" ]; then
-                echo "[ERROR] SSH key generation failed"
-                exit 1
-        fi
-        echo "Please use this key to allow PCAP files transfert via scp:"
-        echo "--- Cut Here ---"
-        cat /root/.ssh/id_rsa.pub
-        echo "--- Cut Here ---"
-
-        # Create the cronjob
-        echo "*/5 * * * * root (/pcap_transfer.sh \"$SCP_ARGUMENTS\" \"$SCP_TARGET\" >>/var/log/cron.log 2>&1)" >/etc/cron.d/pcap
-        chmod 0644 /etc/cron.d/pcap
-        touch /var/log/cron.log
-
-        # Create the supervisord.conf
-        cat <<__CONFIG__ >/opt/supervisor.conf
-[supervisord]
-nodaemon=true
-
-[group:pcap]
-programs=pcap_tcpdump,pcap_cron
-
-[program:pcap_tcpdump]
-command=/usr/sbin/tcpdump -n -Z nobody -i $PCAP_INTERFACE -s $PCAP_CAPTURE_SIZE -G $PCAP_FILE_SIZE -W $PCAP_FILE_ROTATE $PCAP_BPF_FILTER -w /data/$PCAP_SENSOR_NAME-%%Y%%m%%d%
-%H%%M.pcap
-autorestart=true
-
-[program:pcap_cron]
-command=/usr/sbin/cron -f
-autorestart=true
-__CONFIG__
-        chown nobody /data
-
-	# Setup completed
-	rm /.firstboot
+#
+# Start the FPC sensor (tcpdump + socat)
+#
+if [ -z "$PCAP_INTERFACE" ]; then
+	export PCAP_INTERFACE=any
 fi
 
-/usr/bin/supervisord -c /opt/supervisor.conf
+if [ -z "$PCAP_SNAPLEN" ]; then
+	export PCAP_SNAPLEN=0
+fi
+
+if [ -z "$PCAP_BPF_FILTER" ]; then
+	export PCAP_BPF_FILTER=""
+fi
+
+# Generate a self-signed certificate for Socat
+openssl genrsa -out /sensor.key 1024
+openssl req -new \
+        -key /sensor.key \
+        -x509 \
+        -days 365 \
+        -subj "/C=BE/ST=Brussels/L=Brussels/O=SOC/CN=fpc-sensor" \
+        -out /sensor.crt
+cat /sensor.key /sensor.crt >/sensor.pem
+chmod 600 /sensor.key /sensor.pem
+
+while true
+do
+        echo "Sniffing packets on $PCAP_INTERFACE..."
+        /usr/sbin/tcpdump -n -Z nobody -i $PCAP_INTERFACE -s $PCAP_SNAPLEN -w - $PCAP_BPF_FILTER \
+		| /usr/bin/socat - OPENSSL:$INDEXER,cert=/sensor.pem,verify=0,forever,retry=10,interval=5
+        echo "Restarting..."
+done
--- sensor/pcap_transfer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCP_ARGUMENTS=$1
-SCP_TARGET=$2
-find /data -name "*.pcap" -mmin +5 -print | while read PCAP_FILE
-do
-        scp -q $SCP_ARGUMENTS $PCAP_FILE $SCP_TARGET && rm $PCAP_FILE && echo "Successfully exported $PCAP_FILE"
-done
